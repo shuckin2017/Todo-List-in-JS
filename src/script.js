d477a76d125a7ebea054e19b704124a322cfa39b
shuckin2017 <shuckin@mail.ru>
2020-11-08 11:38:22 +0300

    Реализовано хранение в localStorage, проблема с выводом / Подготовка к ДЗ [JSD-7]

--- src/script.js
+++ src/script.js
@@ -1,40 +1,85 @@
 const addMessage = document.querySelector('#newTask'),
       addButton = document.querySelector('#btn_task'),
       todos = document.querySelector('.todo_list'),
+      todoItem = document.querySelector('.todo_item'),
       popup = document.querySelector('.popup'),
       closePopup = document.querySelector('.close');
 
+const todosData = [];
+
+// const url = 'https://jsonplaceholder.typicode.com/todos';
+// const myRequest = new XMLHttpRequest();  
+
+// function sendTest(){
+//   return new Promise((resolve, regect) => {
+//     myRequest.open('GET', url);
+//     myRequest.send();
+//     myRequest.onload = ()=> {
+//       const status = myRequest.status;
+//       if(status < 400) {
+//         resolve(myRequest.response);
+//       } else {
+//         console.log('Ошибка запроса');
+//       }
+//     };
+//   });
+// };
+
+// const testPromise = sendTest();
+
+// testPromise.then((data) => {
+//   JSON.parse(data).forEach((key) => {
+//     dataTodos = key.title;
+    
+//   });   
+// });
+checkTodo();
+loadTodos();
 
 addButton.addEventListener('click', () => {
-  if( addMessage.value == "" ){
+  noneTodo();
+});
+
+addMessage.addEventListener("keydown", (e) => {
+  if (e.keyCode == 13) {
+    noneTodo();
+  }
+});
+
+function noneTodo() {
+  if( addMessage.value == "" ) {
     popup.classList.remove("d-none");
   } else {
     addTodo();
-    addMessage.value = '';
   }
-});
-
-addEventListener("keydown", (e) => {
-  if (e.keyCode == 13) {
-    if( addMessage.value == "" ){
-      popup.classList.remove("d-none");
-    } else {
-      addTodo();
-      addMessage.value = '';
-    }
-  }
-});
-
+};
 
 function addTodo() {
   const todo = getTodo();
   todos.appendChild(todo);
+  todosData.push(addMessage.value);
+  addTodoJson();
+  addMessage.value = '';
+};
+
+function addTodoJson() {
+  if(todosData != null ) {
+    localStorage.setItem('todosData', JSON.stringify(todosData));
+  }
+};
+
+function loadTodos() {
+  if(localStorage.getItem('todosData')){
+    data = JSON.parse(localStorage.getItem('todosData'));
+    data.forEach((item) => {
+      console.log(item);
+    });
+  }
 };
 
 const getTodo = () => {
   const todo = document.createElement('li');
   todo.className = 'todo_item';
-  // todo.className = 'list-group-item';
   const input = document.createElement('input');
   // input.id = 'item_' todo id
   input.type = 'checkbox';
@@ -48,6 +93,7 @@ const getTodo = () => {
   btnDelete.innerHTML = '&times';
   btnDelete.addEventListener('click', () => {
     todos.removeChild(todo);
+    // localStorage.removeItem('todos', addMessage.innerHTML);
   });
   todo.appendChild(input);
   todo.appendChild(label);
@@ -55,11 +101,16 @@ const getTodo = () => {
   return todo;
 };
 
-
 closePopup.addEventListener("click", (e) => {
   e.preventDefault();
   popup.classList.add("d-none");
-  
 });
 
 
+function checkTodo() {
+  if(!todos.innerHTML == null) {
+    console.log("список пуст");
+  } else {
+    console.log("список");
+  }
+};
