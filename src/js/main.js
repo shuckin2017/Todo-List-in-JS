7e053bf5bdfc7420cb8bb951f6da90c680efce70
shuckin2017 <shuckin@mail.ru>
2020-11-29 02:22:37 +0300

    Выполнено большее количество доработак  [JSD-28]

--- src/js/main.js
+++ src/js/main.js
@@ -13,9 +13,13 @@ const addMessage = document.querySelector('#newTask'),
       checkbox = document.querySelector('input[type=checkbox]'),
       popup = document.querySelector('.popup'),
       spinner = document.querySelector('.spinner__wrapper'),
+      lengthActive = document.querySelector('.todo-info__item--active'),
+      lengthСompleted = document.querySelector('.todo-info__item--copmleted'),
+      lengthAll = document.querySelector('.todo-info__item--all'),
       closePopup = document.querySelector('.close');
 
-let todosData;
+let todosData,
+    todoItemElem = [];
 
  if(!localStorage.todosData ) {
     todosData = [];
@@ -33,28 +37,48 @@ function Task(description, dateCreation) {
 // Set todo list in LocalStorage
 const updateStorage = () => localStorage.setItem('todosData', JSON.stringify(todosData));
 
+const filterTasks = () => {
+  const activeTask = todosData.length && todosData.filter(item => item.completed == false);
+  const completedTask = todosData.length && todosData.filter(item => item.completed == true);
+  lengthActive.innerHTML = activeTask.length;
+  lengthСompleted.innerHTML = completedTask.length;
+  todosData = [...activeTask, ...completedTask];
+  lengthAll.innerHTML = todosData.length;
+};
+
 const fillList = () => {
   todos.innerHTML = '';
   if(todosData.length > 0) {
+    filterTasks();
     todosData.forEach((item, index) => {
-      createTodo(item);
+      createTodo(item, index);
     });
+    todoItemElem = document.querySelectorAll('.todo_item');
+    
   }
 };
 
-const createTodo = (item) => {
-  todos.appendChild(createTemplate(item));
+
+const createTodo = (item, index) => {
+  todos.appendChild(createTemplate(item, index));
   addMessage.value = '';
   checkTodo();
 };
 
-const createTemplate = (item) => {
+const createTemplate = (item, index) => {
   const todo = document.createElement('li'),
         input = document.createElement('input'),
         label = document.createElement('label'),
         btnDelete = document.createElement('button');
 
   todo.className = 'todo_item';
+  input.addEventListener('click', () => {
+    completedTask(index);
+    if(input.checked) {
+      todo.className = 'todo_item completed';
+
+    }
+  });
   input.type = 'checkbox';
   input.value = 'Новая задача';
   item.completed ? input.setAttribute('checked', '') : '';
@@ -65,11 +89,22 @@ const createTemplate = (item) => {
   btnDelete.type = 'button';
   btnDelete.innerHTML = '&times';
   btnDelete.addEventListener('click', () => {
-    todos.removeChild(todo);
+    todoItemElem[index].classList.add('deletedItem');
+    setTimeout(() => {
+      removeTask(index);
+    }, 900);
   });
   todo.appendChild(input);
   todo.appendChild(label);
   todo.appendChild(btnDelete);
+  // todo.addEventListener('click', () => {
+  //   if(todo.childNodes.length == 3) {
+  //     todo.appendChild(showInfoTask(item)); 
+  //   } else {
+  //     todo.lastChild.remove();
+  //   }
+
+  // });
   return todo;
 };
 
@@ -77,8 +112,22 @@ const getList = () => {
   if(addMessage.value) {
     todosData.push(new Task(addMessage.value, service.getDateCreation()));
   }
+  checkTodo();
   updateStorage();
   fillList();
+};
+
+const completedTask = (index) => {
+  todosData[index].completed = !todosData[index].completed;
+  todosData[index].completed ? todoItemElem[index].classList.add('completed') : todoItemElem[index].classList.remove('completed');
+  updateStorage();
+};
+
+const removeTask = (index) => {
+  todosData.splice(index, 1);
+  updateStorage();
+  fillList();
+  checkTodo();
 };
 
 getList();
@@ -105,7 +154,6 @@ deleteAllButton.addEventListener('click', () => {
   }, 1000);
 });
 
-
 const loadTodo = () => {
   service.elemShow(spinner, 0);
   const loadTodo = service.sendTest();
@@ -120,22 +168,29 @@ const loadTodo = () => {
   });
 };
 
-/* date creation todos*/
-
-function checkComplated() {
-  if(checkbox.checked) {
-    console.log('ffff');
-  }
-}
-
-// checkbox.onclick = function(){
-//   if (checkbox.checked) { alert("Чекбокс нажат -вариант №1"); } else { alert("Чекбокс не нажат-вариант №1"); }
-//   }
-
 function checkTodo() {
   if(todos.childElementCount) {
     service.elemHide(listEmpty, 0);
     return;
   } 
   service.elemShow(listEmpty, 0);
-};
+}
+
+function showInfoTask(item) {
+  const panelTask = document.createElement('div'),
+        panelTask__info = document.createElement('div'),
+        panelTask__title = document.createElement('div'),
+        panelTask__date = document.createElement('div');
+        
+  panelTask.classList.add('panelTask');
+  panelTask__info.classList.add('panelTask__info');
+  panelTask__title.classList.add('panelTask__title');
+  panelTask__title.innerHTML = item.title;
+  panelTask__date.classList.add('panelTask__date');
+
+  panelTask__info.appendChild(panelTask__title);
+  panelTask__info.appendChild(panelTask__date);
+  panelTask.appendChild(panelTask__info);
+
+  return panelTask;
+}
